95553e8d816254f5a7dfe98c05c4fbce7e28a317
jeanmon <jean@aztec-labs.com>
2025-08-25 12:28:23 +0000

    Memory constraints

--- barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
@@ -96,6 +96,7 @@
 #include "relations/lookups_keccakf1600.hpp"
 #include "relations/lookups_l1_to_l2_message_exists.hpp"
 #include "relations/lookups_l1_to_l2_message_tree_check.hpp"
+#include "relations/lookups_memory.hpp"
 #include "relations/lookups_merkle_check.hpp"
 #include "relations/lookups_note_hash_tree_check.hpp"
 #include "relations/lookups_notehash_exists.hpp"
@@ -133,10 +134,10 @@ namespace bb::avm2 {
 
 struct AvmFlavorVariables {
     static constexpr size_t NUM_PRECOMPUTED_ENTITIES = 133;
-    static constexpr size_t NUM_WITNESS_ENTITIES = 2936;
-    static constexpr size_t NUM_SHIFTED_ENTITIES = 317;
+    static constexpr size_t NUM_WITNESS_ENTITIES = 2959;
+    static constexpr size_t NUM_SHIFTED_ENTITIES = 323;
     static constexpr size_t NUM_WIRES = NUM_WITNESS_ENTITIES + NUM_PRECOMPUTED_ENTITIES;
-    static constexpr size_t NUM_ALL_ENTITIES = 3386;
+    static constexpr size_t NUM_ALL_ENTITIES = 3415;
 
     // Need to be templated for recursive verifier
     template <typename FF_>
@@ -442,6 +443,11 @@ struct AvmFlavorVariables {
         lookup_l1_to_l2_message_exists_l1_to_l2_msg_leaf_index_in_range_relation<FF_>,
         lookup_l1_to_l2_message_exists_l1_to_l2_msg_read_relation<FF_>,
         lookup_l1_to_l2_message_tree_check_merkle_check_relation<FF_>,
+        lookup_memory_range_check_limb_0_relation<FF_>,
+        lookup_memory_range_check_limb_1_relation<FF_>,
+        lookup_memory_range_check_limb_2_relation<FF_>,
+        lookup_memory_range_check_write_tagged_value_relation<FF_>,
+        lookup_memory_tag_max_bits_relation<FF_>,
         lookup_merkle_check_merkle_poseidon2_read_relation<FF_>,
         lookup_merkle_check_merkle_poseidon2_write_relation<FF_>,
         lookup_note_hash_tree_check_merkle_check_relation<FF_>,
